2d2fc06a2fe6e732d55b35d05af46c2e2108ae11
Rohan Bendapudi <datanteater@gmail.com>
2026-02-09 11:27:39 +0100

    created a get statistics function that will get vllm metrics from an api request --> also started adding doc strings

--- src/benchmark_test.py
+++ src/benchmark_test.py
@@ -5,10 +5,25 @@ import numpy as np
 import os
 from dotenv import load_dotenv
 
+load_dotenv()
 
-async def run_request():
 
-    load_dotenv()
+def run_request(
+    model: str = "Qwen/Qwen3-0.6B",
+    dataset: str = "San Francisco is a",
+    max_tokens: int = 200,
+    temperature: float = 0,
+):
+    """
+    Creates an API request to the vLLM based on the specified model, dataset, max token window, and temperature.
+
+    Returns a JSON response from REST API request.
+
+    model: str - which vLLM model --> depends on the vLLM model that's currently serving
+    dataset: str - string of the dataset that will be sent to vLLM
+    max_tokens: int - the integer of maximum tokens for response window
+    temperature: float value for model temperature
+    """
 
     IP_ADDRESS = os.getenv("IP_ADDRESS")
 
@@ -17,21 +32,74 @@ async def run_request():
     headers = {"Content-Type": "application/json"}
 
     data = {
-        "model": "Qwen/Qwen3-0.6B",
-        "prompt": "San Francisco is a",
-        "max_tokens": 7,
-        "temperature": 0,
+        "model": f"{model}",
+        "prompt": f"{dataset}",
+        "max_tokens": max_tokens,
+        "temperature": temperature,
     }
 
     print(url)
 
     response = requests.post(url, json=data, headers=headers, verify=False)
-    print(response)
+
+    if response.status_code == 200:
+        return response
+    return None
+
     # response_content = json.loads(response.content)
     # latency_times = response_content["kv_transfer_params"]
     # print(f"Prefill Time: {latency_times["prefill_time"]}")
     # print(f"Decode Time: {latency_times["decode_time"]}")
 
 
+def get_statistics(response):
+    """
+    Gets vLLM metrics from the "kv_transfer_params" from the API request response, by loading
+    json into a dict -- and getting dictionary matching "kv_transfer_params".
+
+
+
+    response: Response object - response object from api rquest
+    vllm_request_metrics: dict - dictionary with vllm request metrics
+    """
+
+    response_content = json.loads(response.content)
+    vllm_request_metrics = response_content["kv_transfer_params"]
+    return vllm_request_metrics
+
+
+def send_to_fluentbit(metrics, host="fluentbit", port=9880, tag="vllm.metrics"):
+    url = f"http://{host}:{port}/{tag}"  # FB http input accepts path as tag
+    # Fluent Bit likes JSON objects or arrays of objects
+    r = requests.post(url, json=metrics, timeout=2)
+    r.raise_for_status()
+
+
+def send_event(response):
+    # get environment variables
+    host = os.getenv("host")
+    port = os.getenv("port")
+    tag = os.getenv("vllm.metrics")
+
+    vllm_request_metrics = get_statistics(response)
+    send_to_fluentbit(vllm_request_metrics, host=host, port=port, tag=tag)
+
+
+def create_event():
+    # get environment variables
+    model = os.getenv("model")
+    dataset = os.getenv("dataset")
+    max_tokens = os.getenv("max_tokens")
+    temperature = os.getenv("temperature")
+
+    # runs the request
+    response = run_request(
+        model=model, dataset=dataset, max_tokens=max_tokens, temperature=temperature
+    )
+
+    # gets the statistic from the response, creates an event, and then sends it to fluent bit
+    send_event(response)
+
+
 if __name__ == "__main__":
-    asyncio.run(run_request())
+    asyncio.run(run_request)
